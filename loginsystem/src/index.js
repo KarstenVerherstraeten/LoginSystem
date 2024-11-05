6584f0d84133bc6355a5dca817279429c5120f84
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-05 13:07:32 +0100

    Add @auth0/auth0-react and react-router-dom dependencies

--- loginsystem/src/index.js
+++ loginsystem/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Auth0Provider
+      domain="dev-530wa2fzwh12ouo1.us.auth0.com"
+      clientId="RcYtAqsBDoPZgYLaLr9Y1wsgBmLzIssi"
+      authorizationParams={{
+        redirect_uri: window.location.origin + '/dashboard',
+      }}
+    >
+      <App />
+    </Auth0Provider>,
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
