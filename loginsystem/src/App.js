cd59cc09b9d2b8a29fa2510ba876bbc3840a1deb
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-06 10:06:46 +0100

    added update user page

--- loginsystem/src/App.js
+++ loginsystem/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard'; // Assuming you have a Dashboard component for the dashboard page
 import Home from './pages/Home'; // Assuming you have a Home component for the landing page
+import UpdateUser from './pages/UpdateUser'; // Assuming you have an UpdateUser component for the update user page
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/update-user" element={<UpdateUser />} />
       </Routes>
     </Router>
   );
